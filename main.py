c25b258e29cbb230667fc7a2c2d90688687adfd2
Sergei <sjbilardi@gmail.com>
2016-03-12 00:50:19 -0500

    Added dictionaries for "epslatex"
    Added color, line and point type dictionaries to “gnuplot” function.
    These dictionaries are for use with the “epslatex” terminal.

--- main.py
+++ main.py
@@ -21,12 +21,16 @@ class FigParams:
 class PlotParams:
 	'''For holding the plotting parameters for Gnuplot'''
 
-	def __init__(self, plot_type='lines', color='black', xlim='none', ylim='none',
+	def __init__(self, plot_type='lines',line_type='solid', point_type='solid circle', 
+				 color='black', xlim='none', ylim='none', rotate_tick_vals='none',
 				 error_bars=False, function='sin(x)'):
 
 		self.plot_type = plot_type
+		self.line_type = line_type
+		self.point_type = point_type
 		self.xlim = xlim
 		self.ylim = ylim
+		self.rotate_tick_vals = rotate_tick_vals
 		self.error_bars = error_bars
 
 
@@ -34,7 +38,17 @@ def gnuplot(fig_params, plot_params, x, y, output):
 	'''Will pass arguments to Gnuplot to 
 	   for plotting'''
 
+	# These dictionaries are for the use with "epslatex"
+	ColorParams = {'black': -1, 'purple': 1, 'green': 2, 'light_blue': 3,
+	 			   'orange': 4, 'yellow': 5, 'dark_blue': 6, 'red': 7}
+	PointParams = {'plus': 1, 'cross': 2, 'plus and cross': 3, 'square and dot': 4,
+				   'solid square': 5, 'circle and dot': 6, 'solid circle': 7,
+				   'triangle and dot': 8, 'solid triangle': 9, 'inv solid triangle': 11}
+	LineDashType = {'none': 0, 'single long': 1, 'single short': 2, 'dash dot': 3, 
+					'double dot': 4}
 
+
+	
 	return 0
 
 def main():
