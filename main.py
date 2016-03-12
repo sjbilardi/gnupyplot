e471fcf22bb65da99d2ced83a6527f30325e0ea1
Sergei <sjbilardi@gmail.com>
2016-03-12 04:23:50 -0500

    Attempted to plot discreet data
    This is still being worked on

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 class FigParams:
 	'''For holding the figure parameters to be passed to Gnuplot.'''
 
-	def __init__(self, terminal='epslatex', fig_size=['8', '8'], units='cm', 
+	def __init__(self, terminal='epslatex', fig_size=[8, 8], units='cm', 
 			  	 color=True, colortext=True, border_line='solid'):
 
 		self.terminal = terminal
@@ -22,13 +22,18 @@ class FigParams:
 class PlotParams:
 	'''For holding the plotting parameters for Gnuplot'''
 
-	def __init__(self, plot_type='lines',line_type='solid', point_type='solid circle', 
-				 color='black', xlim='none', ylim='none', rotate_tick_vals='none',
-				 error_bars=False, function='sin(x)', fit_type='none'):
+	def __init__(self, plot_type='lines', color='black', line_type='solid', 
+				 line_width=2, point_type='solid circle', notitle=True, 
+				 xlim='none', ylim='none', rotate_tick_vals='none',
+				 error_bars=False, function=None, fit_type='none'):
 
 		self.plot_type = plot_type
+		self.color = color
+		self.notitle = notitle
 		self.line_type = line_type
+		self.line_width = line_width
 		self.point_type = point_type
+		self.function = function
 		self.xlim = xlim
 		self.ylim = ylim
 		self.rotate_tick_vals = rotate_tick_vals
@@ -42,7 +47,7 @@ def gnuplot(fig_params, plot_params, x, y, output_file):
 
 	if fig_params.terminal == 'epslatex':
 		# These dictionaries are for the use with "epslatex"
-		ColorParams = {'black': -1, 'purple': 1, 'green': 2, 'light_blue': 3,
+		ColorParams = {'black': 8, 'purple': 1, 'green': 2, 'light_blue': 3,
 		 			   'orange': 4, 'yellow': 5, 'dark_blue': 6, 'red': 7}
 		PointParams = {'plus': 1, 'cross': 2, 'plus and cross': 3, 'square and dot': 4,
 					   'solid square': 5, 'circle and dot': 6, 'solid circle': 7,
@@ -50,47 +55,63 @@ def gnuplot(fig_params, plot_params, x, y, output_file):
 		LineDashType = {'none': 0, 'single long': 1, 'single short': 2, 'dash dot': 3, 
 						'double dot': 4}
 	
-	
-		term_set = ('set terminal '+fig_params.terminal+' size '+fig_params.fig_size[0]
-					+fig_params.units+', '+fig_params.fig_size[1]+fig_params.units)
+		# Set figure parameters
+		term_set = ('set terminal '+fig_params.terminal+' size '+str(fig_params.fig_size[0])
+					+fig_params.units+', '+str(fig_params.fig_size[1])+fig_params.units)
+
 		if fig_params.color == True:
 			term_set += ' color'
 		if fig_params.colortext == True:
 			term_set += ' colortext'
 		term_set = term_set + ' ' + fig_params.border_line+'; '
 		print(term_set)
-		output = 'set output ' + '\'' + output_file + '\'' + ';'
+
+		# Set output file parameters
+		output = 'set output ' + '\'' + output_file + '\'' + '; '
 		print(output)
+
+		# Set plotting parameters
+		plot = ('plot \'-\' using 1:2 with '+plot_params.plot_type+' lt '+str(PointParams[plot_params.point_type])
+				+' lc '+str(ColorParams[plot_params.color]))
+		if plot_params.notitle == True:
+			plot = plot + ' notitle'
+		plot += ';\n'
+		print(plot)
+
+		# Plot Data
+		data = ' '
+		for val in np.arange(len(x)):
+			if val == len(x) - 1:
+				data = data + str(x[val]) + '\t' + str(y[val]) + ';'
+				print(str(x[val]) + '\t' + str(y[val]) + ';')
+			else:
+				data = data + str(x[val]) + '\t' + str(y[val]) + '; '
+				print(str(x[val]) + '\t' + str(y[val]) + '; ')
+		# data = plot_params.function
+		cmd = ('gnuplot -e \"'+term_set+output+plot+data+'\"')
 		# launch gnuplot
-		call('gnuplot -e \"'+term_set+output+' plot sin(x) notitle\"', shell=True)
-		# process = subprocess.Popen('gnuplot', stdout=subprocess.PIPE)
-		# output = process.communicate()[0]
-		# print(output)
-		# # set output
-		# call([output])
-		# call('plot', 'sin(x)')
-	return 0
+		print(cmd)
+		call(cmd, shell=True)
 
-def main():
-	# %% Make x and y values
-	w_0 = 2 # set angular frequency
-	t = np.linspace(-10, 10, 500)
-	y = np.cos(w_0*t)
+	return data
 
-	# %% Plot above with Matplotlib
-	# plt.close('all')
-	# sns.set_style("white")
-	# sns.set_style("ticks")
-	# plt.figure(figsize=(8,8))
-	# plt.plot(t, y)
-	# plt.show()  
-	
-	# %% Plot with Gnuplot
-	fig_params = FigParams()
-	plot_params = PlotParams()
-	output_file = 'plots/output.tex'
-	gnuplot(fig_params=fig_params, plot_params=plot_params,
-			x=t, y=y, output_file=output_file)
- 
-main()
+# %% Make x and y values
+w_0 = 2 # set angular frequency
+t = np.linspace(-10, 10, 1)
+y = np.cos(w_0*t)
+# %% Plot above with Matplotlib
+# plt.close('all')
+# sns.set_style("white")
+# sns.set_style("ticks")
+# plt.figure(figsize=(8,8))
+# plt.plot(t, y)
+# plt.show()  
+
+# %% Plot with Gnuplot
+fig_params = FigParams()
+plot_params = PlotParams()
+plot_params.function = 'sin(x)'
+output_file = 'plots/output.tex'
+data = gnuplot(fig_params=fig_params, plot_params=plot_params,
+		x=t, y=y, output_file=output_file)
 	
--- test_files/gnu_test.gp
+++ test_files/gnu_test.gp
@@ -1,9 +1,8 @@
-set terminal epslatex size 9cm,7cm color colortext solid 
-set output 'test_plot.tex'
-
-plot '-' using 1:2 with points lt 7 lc 7 notitle
-0 0
-1 1
-2 2
-3 3
-4 4
+set terminal epslatex size 8cm, 8cm color colortext solid;
+set output 'output.tex';
+plot '-' using 1:2 with lines lt 7 lc 8 notitle;
+ -10.0	0.408082061813;
+-5.0	-0.839071529076;
+0.0	1.0;
+5.0	-0.839071529076;
+10.0	0.408082061813;
