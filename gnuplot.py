9952d5222c7a5ea9ab00daa3869963d1f13027a2
Sergei <sjbilardi@gmail.com>
2016-03-13 16:58:18 -0400

    Added files
    Added:
    
    - __init__.py file
    - updated gitignore to ignore python cache files and gnuplot example
    code files

--- gnuplot.py
+++ gnuplot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 from subprocess import call
 import subprocess
@@ -148,16 +149,7 @@ def gnuplot(fig_params, plot_params, data, output_file):
 		plot = plot + ' notitle'
 	plot += ';'
 	print(plot)	
-	# # Pipe plot Data
-	# data = ' '
-	# for val in np.arange(len(x)):
-	# 	if val == len(x) - 1:
-	# 		data = data + str(x[val]) + '\t' + str(y[val]) + ';'
-	# 		print(str(x[val]) + '\t' + str(y[val]) + ';')
-	# 	else:
-	# 		data = data + str(x[val]) + '\t' + str(y[val]) + '; '
-	# 		print(str(x[val]) + '\t' + str(y[val]) + '; ')
-	# data = plot_params.function
+	# make final command strings
 	if fig_params.terminal == 'epslatex' or fig_params.terminal == 'eps':
 		cmd = ('gnuplot -e \"'+term_set+output+args+plot+' \"')
 	elif fig_params.terminal == 'xterm':
@@ -166,31 +158,4 @@ def gnuplot(fig_params, plot_params, data, output_file):
 	print(cmd)
 	call(cmd, shell=True)
 	return True
-
-# %% Make x and y values
-w_0 = 2 # set angular frequency
-t = np.linspace(-10, 10, 10000)
-y = np.cos(w_0*t)
-# %% Plot above with Matplotlib
-# plt.close('all')
-# sns.set_style("white")
-# sns.set_style("ticks")
-# plt.figure(figsize=(8,8))
-# plt.plot(t, y)
-# plt.show()  
-
-# %% Plot with Gnuplot
-fig_params = FigParams(terminal='eps', fig_size=[8, 8], units='cm', 
-			  	 color=True, colortext=True, standalone=False, border_line='solid')
-
-plot_params = PlotParams(plot_type='lines', color='black', line_type='solid', 
-				 line_width=3, point_type='solid circle', notitle=True, 
-				 line_dash='none', xlim=[-5,5], ylim=[-2,2], rotate_tick_vals=None,
-				 xlabel='X Values', ylabel='Y Values', xtick_label_format='%.2f', 
-				 ytick_label_format='%.2f', error_bars=None, fit_type=None, 
-				 graph_label=None, mxticks=3, myticks=3)
-
-output_file = 'plots/output'
-data = gnuplot(fig_params=fig_params, plot_params=plot_params,
-		data=[t, y], output_file=output_file)
 	
---
+++ plot_example.py
@@ -0,0 +1,22 @@
+import numpy as np
+from gnuplot import FigParams, PlotParams, gnuplot
+
+# %% Make x and y values
+w_0 = 2 # set angular frequency
+t = np.linspace(-10, 10, 10000)
+y = np.cos(w_0*t)
+
+# %% Plot with Gnuplot
+fig_params = FigParams(terminal='eps', fig_size=[8, 8], units='cm', 
+			  	 color=True, colortext=True, standalone=False, border_line='solid')
+
+plot_params = PlotParams(plot_type='lines', color='black', line_type='solid', 
+				 line_width=3, point_type='solid circle', notitle=True, 
+				 line_dash='none', xlim=[-5,5], ylim=[-2,2], rotate_tick_vals=None,
+				 xlabel='X Values', ylabel='Y Values', xtick_label_format='%.2f', 
+				 ytick_label_format='%.2f', error_bars=None, fit_type=None, 
+				 graph_label=None, mxticks=3, myticks=3)
+
+output_file = 'plots/output'
+data = gnuplot(fig_params=fig_params, plot_params=plot_params,
+		data=[t, y], output_file=output_file)
