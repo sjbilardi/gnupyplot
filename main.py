2f0f8dbf1f766cc16760a4bfee426c1847e98375
Sergei <sjbilardi@gmail.com>
2016-03-13 04:15:36 -0400

    small tweaks

--- main.py
+++ main.py
@@ -10,21 +10,22 @@ class FigParams:
 	'''For holding the figure parameters to be passed to Gnuplot.'''
 
 	def __init__(self, terminal='epslatex', fig_size=[8, 8], units='cm', 
-			  	 color=True, colortext=True, border_line='solid'):
+			  	 color=True, colortext=True, standalone=False, border_line='solid'):
 
 		self.terminal = terminal
 		self.fig_size = fig_size
 		self.units = units
 		self.color = color
 		self.colortext = colortext
+		self.standalone = standalone
 		self.border_line = border_line
 
 class PlotParams:
 	'''For holding the plotting parameters for Gnuplot'''
 
 	def __init__(self, plot_type='lines', color='black', line_type='solid', 
-				 line_width=2, point_type='solid circle', notitle=True, 
-				 xlim='none', ylim='none', rotate_tick_vals='none',
+				 line_width=3, point_type='solid circle', notitle=True, 
+				 line_dash='none', xlim='none', ylim='none', rotate_tick_vals='none',
 				 error_bars=None, fit_type='none'):
 
 		self.plot_type = plot_type
@@ -32,6 +33,7 @@ class PlotParams:
 		self.notitle = notitle
 		self.line_type = line_type
 		self.line_width = line_width
+		self.line_dash = line_dash
 		self.point_type = point_type
 		self.xlim = xlim
 		self.ylim = ylim
@@ -66,31 +68,38 @@ def gnuplot(fig_params, plot_params, data, output_file):
 	PointParams = {'plus': 1, 'cross': 2, 'plus and cross': 3, 'square and dot': 4,
 				   'solid square': 5, 'circle and dot': 6, 'solid circle': 7,
 				   'triangle and dot': 8, 'solid triangle': 9, 'inv solid triangle': 11}
-	LineDashType = {'none': 0, 'single long': 1, 'single short': 2, 'dash dot': 3, 
-					'double dot': 4}
+	LineDashType = {'none': 1, 'single long': 1, 'single short': 3, 'dash dot': 4, 
+					'double dot': 5}
 	
 	# Set figure parameters
-	if fig_params.terminal == 'epslatex':
-		term_set = ('set terminal '+fig_params.terminal+' size '+str(fig_params.fig_size[0])
-					+fig_params.units+', '+str(fig_params.fig_size[1])+fig_params.units)
+	if fig_params.terminal == 'xterm':
+		term_set = ('set terminal '+fig_params.terminal+' persist; ')
 		if fig_params.color == True:
 			term_set += ' color'
 		if fig_params.colortext == True:
 			term_set += ' colortext'
-		term_set = term_set + ' ' + fig_params.border_line+'; '
-		print(term_set)
-		# Set output file parameters
+		if fig_params.standalone == True:
+			term_set += ' standalone'
+	elif fig_params.terminal == 'epslatex':
+		term_set = ('set terminal '+fig_params.terminal+' size '+str(fig_params.fig_size[0])
+					+fig_params.units+', '+str(fig_params.fig_size[1])+fig_params.units)
+	elif fig_params.terminal == 'eps':
+		term_set = ('set size square; set terminal postscript eps size '+str(fig_params.fig_size[0])
+					+fig_params.units+', '+str(fig_params.fig_size[1])+fig_params.units)
+	term_set = term_set + ' ' + fig_params.border_line+'; '
+	print(term_set)
+	# Set output file parameters
+	if fig_params.terminal == 'epslatex':
 		output = 'set output ' + '\'' + output_file + '.tex' + '\'' + '; '
-		print(output)
-
-	elif fig_params.terminal == 'xterm':
-		term_set = ('set terminal '+fig_params.terminal+' persist; ')
+	elif fig_params.terminal == 'eps':
+		output = 'set output ' + '\'' + output_file + '.eps' + '\'' + '; '
+	print(output)
 
 	# Set plotting parameters
 	# If a function is given
 	if len(data) == 1:
-		plot = ('plot '+data+' using 1:2 with '+plot_params.plot_type+' lt '+str(PointParams[plot_params.point_type])
-				+' lc '+str(ColorParams[plot_params.color]))
+		plot = ('plot '+data+' with '+plot_params.plot_type+' lt '+str(PointParams[plot_params.point_type])
+				+' lc '+str(ColorParams[plot_params.color])+' lw '+str(plot_params.line_width)+' dt '+str(LineDashType[plot_params.line_dash]))
 	# If a list of data is given
 	else:
 		if len(data) == 2:
@@ -98,7 +107,7 @@ def gnuplot(fig_params, plot_params, data, output_file):
 				write_data, data_file = _make_data_file(data=data, output_file=output_file, errors=plot_params.error_bars)
 				if write_data == True:
 					plot = ('plot \''+data_file+'\' using 1:2 with '+plot_params.plot_type+' lt '+str(PointParams[plot_params.point_type])
-							+' lc '+str(ColorParams[plot_params.color]))
+							+' lc '+str(ColorParams[plot_params.color])+' lw '+str(plot_params.line_width)+' dt '+str(LineDashType[plot_params.line_dash]))
 				else:
 					print('Error writing data to file.')
 					return 'Error writing to file.'
@@ -122,7 +131,7 @@ def gnuplot(fig_params, plot_params, data, output_file):
 	# 		data = data + str(x[val]) + '\t' + str(y[val]) + '; '
 	# 		print(str(x[val]) + '\t' + str(y[val]) + '; ')
 	# data = plot_params.function
-	if fig_params.terminal == 'epslatex':
+	if fig_params.terminal == 'epslatex' or fig_params.terminal == 'eps':
 		cmd = ('gnuplot -e \"'+term_set+output+plot+' \"')
 	elif fig_params.terminal == 'xterm':
 		cmd = ('xterm -hold -e gnuplot -e \"'+term_set+plot+'\"')
@@ -133,7 +142,7 @@ def gnuplot(fig_params, plot_params, data, output_file):
 
 # %% Make x and y values
 w_0 = 2 # set angular frequency
-t = np.linspace(-10, 10, 1000)
+t = np.linspace(-10, 10, 10000)
 y = np.cos(w_0*t)
 # %% Plot above with Matplotlib
 # plt.close('all')
@@ -144,12 +153,14 @@ y = np.cos(w_0*t)
 # plt.show()  
 
 # %% Plot with Gnuplot
-fig_params = FigParams(terminal='xterm', fig_size=[8, 8], units='cm', 
-			  	 color=True, colortext=True, border_line='solid')
-plot_params = PlotParams(plot_type='points', color='black', line_type='solid', 
+fig_params = FigParams(terminal='eps', fig_size=[8, 8], units='cm', 
+			  	 color=True, colortext=True, standalone=True, border_line='solid')
+
+plot_params = PlotParams(plot_type='lines', color='black', line_type='solid', 
 				 line_width=2, point_type='solid circle', notitle=True, 
-				 xlim='none', ylim='none', rotate_tick_vals='none',
+				 line_dash='none', xlim='none', ylim='none', rotate_tick_vals='none',
 				 error_bars=None, fit_type='none')
+
 output_file = 'plots/output'
 data = gnuplot(fig_params=fig_params, plot_params=plot_params,
 		data=[t, y], output_file=output_file)
--- test_files/gnu_test.gp
+++ test_files/gnu_test.gp
@@ -1,6 +1,7 @@
-set terminal epslatex size 8cm, 8cm color colortext solid;
-set output 'output.tex';
-plot '-' using 1:2 with lines lt 7 lc 8 notitle;
+set size square
+set terminal postscript eps size 8cm, 8cm color colortext solid;
+set output 'output.eps';
+plot '-' using 1:2 with lines lt 7 lc 8 lw 2 dt 5 notitle;
  -10.0	0.408082061813;
 -5.0	-0.839071529076;
 0.0	1.0;
